acd92a85e6d867fdf202f50bdb1395b3303fe542
Mukhtar Bahadory <mukhtar5.bahadory5@gmail.com>
2025-11-06 11:46:41 -0800

    fixed imports

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -4,7 +4,7 @@ import { createServer } from 'http';
 import express from 'express';
 import cors from 'cors';
 import { monitor } from '@colyseus/monitor';
-import { GameRoom } from './rooms/GameRoom';
+import { GameRoom } from './rooms/GameRoom.js';
 
 const port = Number(process.env.PORT) || 2567;
 const app = express();
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,3 @@
-export * from './constants';
-export * from './types';
-export * from './schema/GameState';
+export * from "./constants.js"
+export * from "./types.js"
+export * from "./schema/GameState.js"
